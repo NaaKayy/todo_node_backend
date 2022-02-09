1bd05537b5bf634961f3f7b415751c91ff63ad84
NaaKayy <89992467+NaaKayy@users.noreply.github.com>
2022-02-09 20:38:27 +0000

    port changed in app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const todoController = require('./controllers/todoController');
+const PORT = process.env.PORT || 3000;
+
+
+//routes api
+app.use(express.json());
+app.get('/todos', todoController.getallTodo);
+app.post('/todos', todoController.addTodo);
+app.patch('/todos/:todoId', todoController.updateTodoById);
+app.delete('/todos', todoController.deleteTodoById);
+app.get('/todos/:todoId', todoController.getTodoById);
+
+app.listen(PORT, function(){
+    console.log('Server has started to run');
+    mongoose.connect(process.env.LOCAL_DB)
+    .then(function(){
+        console.log('DB is connected');
+    })
+    .catch(function(error){
+        console.log('DB is not connected: ', error.message);
+    })  
+});
+
+
